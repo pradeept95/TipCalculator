2fa608680f448ffeb6931eba97d655cafb7fef35
pradeept95 <pradeep.thapaliya95@gmail.com>
2021-08-21 15:26:02 -0500

    settings

---
+++ Prework/UserKeys.swift
@@ -0,0 +1,17 @@
+//
+//  UserKeys.swift
+//  Prework
+//
+//  Created by Diksha Thakur on 8/21/21.
+//
+
+import Foundation
+
+
+struct defaultTipPercentage {
+    static let optOne = "optOne"
+    static let optTwo = "optTwo"
+    static let optThree = "optThree"
+}
+
+
--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -19,15 +19,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipPercentageView: UIProgressView!
     // var currentTipPercentage = Double(0)
-    var tipPercentage = [0.15, 0.18, 0.20]
+    var tipPercentage = [0.0, 0.0, 0.0]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        billAmountTextField.becomeFirstResponder()
+         
         tipAmountTextField.isHidden = true
         tipCustomAmountLabel.isHidden = true
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        billAmountTextField.becomeFirstResponder()
+        setSettingValue()
     }
     
     
@@ -36,6 +43,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func customTipAmountChanged(_ sender: Any) {
+        
+        let newTipPercentage = Double(tipAmountTextField.text!) ?? 0
+        
+        if(newTipPercentage > 100 || newTipPercentage < 0){
+            let alert = UIAlertController(title: "Invalid Tip %, enter tip percentage between 0 and 100", message: nil, preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+
+                self.tipAmountTextField.becomeFirstResponder()
+                self.tipAmountTextField.text = String("")
+            }))
+
+            self.present(alert, animated: true)
+            
+            return
+        }
+        
         calculateTipAmount()
         updateScreen()
         
@@ -43,31 +67,31 @@ class ViewController: UIViewController {
     
     
     @IBAction func tipPercentageChanged(_ sender: Any) {
+         
         calculateTipAmount()
         updateScreen()
-        
-        let alert = UIAlertController(title: "What's your name?", message: nil, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-
-        alert.addTextField(configurationHandler: { textField in
-            textField.placeholder = "Input tip %..."
-        })
-
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
-
-            if let name = alert.textFields?.first?.text {
-                print("Your name: \(name)")
-            }
-        }))
-
-        self.present(alert, animated: true)
-
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       self.view.endEditing(true)
     }
     
     func calculateTipAmount() {
         
         //get bill amount
         let bill = Double(billAmountTextField.text!) ?? 0
+        
+        if(bill == 0){
+            let alert = UIAlertController(title: "Please, enter your bill amount!", message: nil, preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+
+                self.billAmountTextField.becomeFirstResponder()
+            }))
+
+            self.present(alert, animated: true)
+            return
+        }
         
         var tip = Double(0)
         let selectedIndex = Int(tipControl.selectedSegmentIndex)
@@ -82,15 +106,13 @@ class ViewController: UIViewController {
           
         let total = bill + tip
             
-        let formatter = NumberFormatter()
-        formatter.locale = Locale.current
-        formatter.numberStyle = .currency
+      
         
         //update tip amount
-        tipAmountLabel.text = formatter.string(from: tip as NSNumber)
+        tipAmountLabel.text = formatCurrency(amt: tip)
         
         //update total amount
-        totalLabel.text = formatter.string(from: total as NSNumber)
+        totalLabel.text =  formatCurrency(amt: total)
     }
     
     func updateScreen(){
@@ -100,7 +122,7 @@ class ViewController: UIViewController {
         if(selectedIndex != 3){
             tipAmountTextField.isHidden = true
             tipCustomAmountLabel.isHidden = true
-            tipAmountTextField.text = String(0)
+            tipAmountTextField.text = String("")
             
             tipPercentageView.setProgress( Float(tipPercentage[selectedIndex]), animated: true)
             
@@ -116,5 +138,57 @@ class ViewController: UIViewController {
               
         }
     }
+    
+    func formatCurrency(amt : Double) -> String {
+        
+        let formatter = NumberFormatter()
+        formatter.locale = Locale.current
+        formatter.numberStyle = .currency
+        
+        return formatter.string(from: amt as NSNumber) ?? String(0)
+    }
+    
+    func setSettingValue(){
+        let defaults = UserDefaults.standard
+        
+        //set first option
+        if let firstOpt = defaults.string(forKey: defaultTipPercentage.optOne) {
+            tipPercentage[0] = Double(firstOpt) ?? 0.0
+        }else{
+            defaults.set("0.15", forKey: defaultTipPercentage.optOne)
+            tipPercentage[0] = 0.15
+        }
+        
+        //set second option
+        if let secondOpt = defaults.string(forKey: defaultTipPercentage.optTwo) {
+            tipPercentage[1] = Double(secondOpt) ?? 0.0
+        }else{
+            defaults.set("0.18", forKey: defaultTipPercentage.optTwo)
+            tipPercentage[1] = 0.18
+        }
+        
+        //set second option
+        if let thirdOpt = defaults.string(forKey: defaultTipPercentage.optThree) {
+            tipPercentage[2] = Double(thirdOpt) ?? 0.0
+        }else{
+            defaults.set("0.20", forKey: defaultTipPercentage.optThree)
+            tipPercentage[2] = 0.20
+        }
+        
+        setPercentageLabel()
+    }
+    
+    
+    func setPercentageLabel(){
+        
+        //changing the title of the tip percentage based on the setting value
+        tipControl?.setTitle(String(format: "%3.0f%%", tipPercentage[0]*100), forSegmentAt: 0)
+        tipControl?.setTitle(String(format: "%3.0f%%",tipPercentage[1]*100), forSegmentAt: 1)
+        tipControl?.setTitle(String(format: "%3.0f%%",tipPercentage[2]*100), forSegmentAt: 2)
+        
+    }
+    
+    
+    //setting area
 }
 
